4f8f684859c3ab48852be7e570f0650d9e282fe6
SalasainAhmad <salasainahmad@gmail.com>
2025-01-29 22:11:45 +0800

    fixed timezone

--- client/schedule.php
+++ client/schedule.php
@@ -403,26 +403,22 @@
                                     <tbody>
 
                                         <?php
-
-
+                                        date_default_timezone_set('Asia/Manila'); // Set timezone to Asia/Manila
                                         $currentDateTime = new DateTime();
 
                                         if ($result->num_rows == 0) {
                                             echo '<tr>
-                                    <td colspan="4">
-                                    <br><br><br><br>
-                                    <center>
-                                    <img src="../img/notfound.svg" width="25%">
-                                    
-                                    <br>
-                                    <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>
-                                    <a class="non-style-link" href="schedule.php"><button  class="login-btn btn-primary-soft btn"  style="display: flex;justify-content: center;align-items: center;margin-left:20px;">&nbsp; Show all Sessions &nbsp;</font></button>
-                                    </a>
-                                    </center>
-                                    <br><br><br><br>
-                                    </td>
-                                    </tr>';
-
+    <td colspan="4">
+    <br><br><br><br>
+    <center>
+    <img src="../img/notfound.svg" width="25%">
+    <br>
+    <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We couldn\'t find anything related to your keywords!</p>
+    <a class="non-style-link" href="schedule.php"><button class="login-btn btn-primary-soft btn" style="display: flex;justify-content: center;align-items: center;margin-left:20px;">&nbsp; Show all Sessions &nbsp;</button></a>
+    </center>
+    <br><br><br><br>
+    </td>
+    </tr>';
                                         } else {
                                             // Display the sessions that have not yet ended
                                             for ($x = 0; $x < $result->num_rows; $x++) {
@@ -436,45 +432,49 @@
                                                     $scheduleid = $row["scheduleid"];
                                                     $title = $row["title"];
                                                     $archiname = $row["archiname"];
-                                                    $scheduledate = date("M d, Y", strtotime($row["scheduledate"]));
-                                                    $scheduletime = date("g:ia", strtotime($row["scheduletime"]));
 
-                                                    // Combine date and time for comparison
-                                                    $scheduleDateTime = new DateTime($row["scheduledate"] . ' ' . $row["scheduletime"]);
+                                                    // Convert scheduled date and time to Asia/Manila timezone
+                                                    $scheduledate = new DateTime($row["scheduledate"] . ' ' . $row["scheduletime"], new DateTimeZone('UTC'));
+                                                    $scheduledate->setTimezone(new DateTimeZone('Asia/Manila'));
+
+                                                    // Formatted date and time for display
+                                                    $formattedDate = $scheduledate->format("M d, Y");
+                                                    $formattedTime = $scheduledate->format("g:ia");
 
                                                     // Skip sessions that have already ended
-                                                    if ($scheduleDateTime < $currentDateTime) {
+                                                    if ($scheduledate < $currentDateTime) {
                                                         continue;
                                                     }
 
                                                     echo '
-                                        <td style="width: 25%;">
-                                                <div class="dashboard-items search-items">
-                                                
-                                                    <div style="width:100%">
-                                                            <div class="h1-search">
-                                                                ' . substr($title, 0, 21) . '
-                                                            </div><br>
-                                                            <div class="h3-search">
-                                                                ' . substr($archiname, 0, 30) . '
-                                                            </div>
-                                                            <div class="h4-search">
-                                                                ' . $scheduledate . '<br>Ends: <b>@' . $scheduletime . '</b> 
-                                                            </div>
-                                                            <br>
-                                                            <a href="booking.php?id=' . $scheduleid . '"><button class="login-btn btn-primary-soft btn" style="padding-top:11px;padding-bottom:11px;width:100%"><font class="tn-in-text">Book Now</font></button></a>
-                                                    </div>
-                                                            
-                                                </div>
-                                            </td>';
-
+            <td style="width: 25%;">
+                <div class="dashboard-items search-items">
+                    <div style="width:100%">
+                        <div class="h1-search">
+                            ' . substr($title, 0, 21) . '
+                        </div><br>
+                        <div class="h3-search">
+                            ' . substr($archiname, 0, 30) . '
+                        </div>
+                        <div class="h4-search">
+                            ' . $formattedDate . '<br>Ends: <b>@' . $formattedTime . '</b> 
+                        </div>
+                        <br>
+                        <a href="booking.php?id=' . $scheduleid . '">
+                            <button class="login-btn btn-primary-soft btn" style="padding-top:11px;padding-bottom:11px;width:100%">
+                                <font class="tn-in-text">Book Now</font>
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </td>';
                                                 }
                                                 echo "</tr>";
                                             }
                                         }
-
                                         ?>
                                     </tbody>
+
 
                                 </table>
                             </div>
